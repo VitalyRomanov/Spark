86ec9f817298764ce2766a5ecf73b48eff072bd8
shishi7 <rus.riko@gmail.com>
2018-12-02 23:18:58 +0300

    Add files via upload
    + predict done
    + normalization by Ans
    + solved problems
    + comments

--- src/main/scala/RSSDemo.scala
+++ src/main/scala/RSSDemo.scala
@@ -13,105 +13,106 @@ import org.apache.spark.ml.feature.{HashingTF, IDF, Tokenizer}
 import scala.collection.mutable.ListBuffer
 import scala.io.Source
 
+
 object RSSDemo {
+  // get list of files inside 'dir' directory
   def getListOfFiles(dir: String): List[String] = {
     val file = new File(dir)
     file.listFiles.filter(_.isFile).map(_.getAbsolutePath).toList
   }
 
+  // convert string to normal format to learn better and increase true predict probability
+  def normalizeString(s: String): String = {
+    s.toLowerCase().replaceAll("\n", " "). // delete new lines
+      replaceAll("[!@#$%^&*()<>,.?/:;'_+*~=|-]", " "). // erase symbols
+      replaceAll("http(.*?)\\s", ""). // delete links
+      replaceAll(" ( )*", " ") // delete >1 spaces
+  }
+
   def main(args: Array[String]) {
-    val durationSeconds = 10
+    val durationSeconds = 15
     val conf = new SparkConf().setAppName("RSS Spark Application").setIfMissing("spark.master", "local[*]")
     val sc = new SparkContext(conf)
     val ssc = new StreamingContext(sc, Seconds(durationSeconds))
     sc.setLogLevel("ERROR")
 
-    val datasetTrainPosDir = "dataset/train/pos"
-    val datasetTrainNegDir = "dataset/train/neg"
-    var trainData : ListBuffer[(Double, String)] = ListBuffer()
-    var id = 0
+    val datasetTrainPosDir = "dataset/train/pos" // path to positive training dataset
+    val datasetTrainNegDir = "dataset/train/neg" // path to negative training dataset
+    var trainData: ListBuffer[(Double, String)] = ListBuffer()
 
+    // get text from files and add to trainData
     getListOfFiles(datasetTrainPosDir).foreach(file => {
       val source = Source.fromFile(file)
-      trainData.append((1.0, source.getLines().mkString))
+      trainData.append((1.0, normalizeString(source.getLines().mkString)))
       source.close()
-      id += 1
     })
 
+    // get text from files and add to trainData
     getListOfFiles(datasetTrainNegDir).foreach(file => {
       val source = Source.fromFile(file)
-      trainData.append((0.0, source.getLines().mkString))
+      trainData.append((0.0, normalizeString(source.getLines().mkString)))
       source.close()
-      id += 1
     })
 
     val spark = SparkSession.builder().appName(sc.appName).getOrCreate()
     val training = spark.createDataFrame(trainData.toList).toDF("label", "text")
 
+    val tokenizer = new Tokenizer().setInputCol("text").setOutputCol("words") // string tokenizer
+    val hashingTF = new HashingTF().setInputCol(tokenizer.getOutputCol).setOutputCol("rawFeatures") // term-frequency
+    val idf = new IDF().setInputCol("rawFeatures").setOutputCol("features") // idf
+    val lr = new LogisticRegression().setMaxIter(10).setRegParam(0.001) // logistic regression
 
-    val tokenizer = new Tokenizer().setInputCol("text").setOutputCol("words")
-    val hashingTF = new HashingTF().setInputCol(tokenizer.getOutputCol).setOutputCol("rawFeatures")
-    val idf = new IDF().setInputCol("rawFeatures").setOutputCol("features")
-    val lr = new LogisticRegression().setMaxIter(10).setRegParam(0.001)
-
-    val pipeline = new Pipeline().setStages(Array(tokenizer, hashingTF, idf, lr))
+    val pipeline = new Pipeline().setStages(Array(tokenizer, hashingTF, idf, lr)) // pipeline: tokenizer -> tf -> idf -> logistic regression
 
     println("Fitting")
-    val model = pipeline.fit(training)
+    val model = pipeline.fit(training) // fitting the model
     println("Fitted")
 
-    val datasetTestPosDir = "dataset/test/pos"
-    val datasetTestNegDir = "dataset/test/neg"
-    var testData : ListBuffer[(Double, String)] = ListBuffer()
-    id = 0
-
-    getListOfFiles(datasetTestPosDir).foreach(file => {
-      val source = Source.fromFile(file)
-      testData.append((1.0, source.getLines().mkString))
-      source.close()
-      id += 1
-    })
-
-    getListOfFiles(datasetTestNegDir).foreach(file => {
-      val source = Source.fromFile(file)
-      testData.append((0.0, source.getLines().mkString))
-      source.close()
-      id += 1
-    })
-
-    val test = spark.createDataFrame(testData.toList).toDF("label", "text")
-
-    val predictions = model.transform(test)
-    val evaluator = new BinaryClassificationEvaluator().setLabelCol("label").setRawPredictionCol("prediction").setMetricName("areaUnderROC")
-
-    val accuracy = evaluator.evaluate(predictions)
-    println(accuracy)
+    //        val datasetTestPosDir = "dataset/test/pos"
+    //        val datasetTestNegDir = "dataset/test/neg"
+    //        var testData : ListBuffer[(Double, String)] = ListBuffer()
+    //
+    //        getListOfFiles(datasetTestPosDir).foreach(file => {
+    //          val source = Source.fromFile(file)
+    //          testData.append((1.0, source.getLines().mkString))
+    //          source.close()
+    //        })
+    //
+    //        getListOfFiles(datasetTestNegDir).foreach(file => {
+    //          val source = Source.fromFile(file)
+    //          testData.append((0.0, source.getLines().mkString))
+    //          source.close()
+    //        })
+    //
+    //        val test = spark.createDataFrame(testData.toList).toDF("label", "text")
+    //
+    //        val predictions = model.transform(test)
+    //        val evaluator = new BinaryClassificationEvaluator().setLabelCol("label").setRawPredictionCol("prediction").setMetricName("areaUnderROC")
+    //
+    //        val accuracy = evaluator.evaluate(predictions)
+    //        println(accuracy)
 
 
     val urlCSV = args(0)
     val urls = urlCSV.split(",")
     // https://queryfeed.net/twitter?q=%23today&title-type=tweet-text-full&order-by=recent&geocode=
-    // TODO: this query gave info for #today and write tweet text in the title, so now we don't need context.(!!! use it !!! you can change hashtag)
-    // SMS: Ruslan, run test data using fitted model and check by hands how our model can predict the result!
     urls.foreach(url => println("URL=" + url))
     val stream = new RSSInputDStream(urls, Map[String, String](
       "User-Agent" -> "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
     ), ssc, StorageLevel.MEMORY_ONLY, pollingPeriodInSeconds = durationSeconds)
 
     stream.foreachRDD(rdd => {
-      rdd.foreach(entry => {
-        //TODO: normalize input data and predict
-        // normalize = lowercase + delete shitty punctuation -> to vectors
-        println("Uri=" + entry.uri)
-        println("Title=" + entry.title)
-        var temp = entry.title.toString().toLowerCase
-        println(temp)
-
-        println()
-      })
       val spark = SparkSession.builder().appName(sc.appName).getOrCreate()
       import spark.sqlContext.implicits._
-      rdd.toDS().show()
+      rdd.toDS().select("title").collect().foreach(text => {
+        // get string and normalize to better format
+        val temp = normalizeString(text.toString())
+        println(temp)
+        // predict and print the answer
+        val test = Seq((1.0, temp)).toDF("label", "text")
+        val prediction = model.transform(test) // predict negative/positive for current text
+        println(prediction.first().getAs("prediction")) // negative = 0.0, positive = 1.0
+      })
     })
 
     // run forever
